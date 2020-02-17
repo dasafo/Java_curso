6e0eab02d4cede81e9d650921f32e17c80dbafd8
David <d.salasforns@gmail.com>
2020-02-17 20:25:49 +0100

    17-02-2020

--- PrimerosPasos/src/Ejemplo_Array2D_16.java
+++ PrimerosPasos/src/Ejemplo_Array2D_16.java
@@ -9,18 +9,18 @@ public class Ejemplo_Array2D_16 {
 		
 		double [][] saldo=new double[6][5];
 		
-		for(int i=0;i<6;i++) {
+		for(int i=0;i<6;i++) { //recorremos las filas
 		
-		saldo[i][0]=20000;  //ponemos la primera columna(0) a 20000
-		acumulado=10000;
-		
-		for(int j=1;j<5;j++) { 
-			acumulado=acumulado+(acumulado*interes);
-			saldo[i][j]=acumulado;		//Ponemos el resto de filas y columnas con el acumulado*interes 
-		
-		}
-		//Cuando termine con la primera culumna(primera iteracion)el interés va cambiando +0.01
-		interes=interes+0.01;
+			saldo[i][0]=20000;  //ponemos la primera columna(0) a 20000
+			acumulado=10000;
+			
+			for(int j=1;j<5;j++) { //recorremos columnas desde la 1
+				acumulado=acumulado+(acumulado*interes);
+				saldo[i][j]=acumulado;		//Ponemos el resto de filas y columnas con el acumulado*interes 
+			
+			}
+			//Cuando termine con la primera culumna(segunda iteracion)el interés va cambiando +0.01
+			interes=interes+0.01;
 		}
 		
 		for(int z=0;z<6;z++) {
